9de8ff1e7a6d0e80686b29627e5681ff7fc21b06
MarianVytak <marianvytak@gmail.com>
2020-03-15 22:18:12 +0200

    Added Navigation anchors.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -4,36 +4,16 @@ $(function () {
 
 
 
-    // $('.section-1__slider_card').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     infinite: true,
-    //     focusOnSelect: true,
-    //     autoplay: true,
-    //     autoplaySpeed: 3000,
-    //     fade: false,
-    //     arrows: false,
-    //     dots: true,
-    //     asNavFor: '.section-1__slider_img',
-    //     appendDots: $('.section-1__slider__counter'),
-    //     customPaging: function (slider, i) {
-    //         return '<span class="slide-current">' + (i + 1) + '</span>' + '<span>из</span>' + '<span class="slide-length">' + slider.slideCount +'</span>';
-    //     },
-    //     responsive: [
-    //         {
-    //             breakpoint: 9999,
-    //             settings: "unslick"
-    //         },
-    //         {
-    //             breakpoint: 1200,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1,
-    //                 arrows: true,
-    //             }
-    //         }
-    //     ]
-    // });
+    // Navigation anchors
+    $('.js-anchor').on('click', function(e){
+        e.preventDefault();
+
+        let navScroll = $(this).attr('href'),
+            navScrollBlock = $(navScroll).offset().top;
+        $('html, body').animate({
+            scrollTop: navScrollBlock
+        }, 2000);
+    });
 
 
 
